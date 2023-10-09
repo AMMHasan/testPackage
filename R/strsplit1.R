05049402d1788458ee477d65e1185739996c4d89
AMMHasan <mahedi.bioinf@gmail.com>
2023-10-09 15:47:32 +0100

    strsplit function documentation updated

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,15 @@
+#' @title split a string
+#'
+#' @param x a character vector
+#' @param split split character
+#'
+#' @return A char vectir
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' strsplit1(x, split = ",")
+#'
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
